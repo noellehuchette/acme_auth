2945c3f924482f369dcdf880a06938e7d74693ce
Noelle Huchette <noelle.huchette@gmail.com>
2022-07-18 20:22:44 +0000

    complete

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const { models: { User }} = require('./db');
+const { models: { User } } = require('./db');
 const path = require('path');
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
-
-app.post('/api/auth', async(req, res, next)=> {
+const requireToken = async (req, res, next) => {
   try {
-    res.send({ token: await User.authenticate(req.body)});
+    const token = req.headers.authorization;
+    const user = await User.byToken(token);
+    req.user = user;
+    next();
   }
-  catch(ex){
-    next(ex);
-  }
-});
+  catch (ex) { next(ex) }
+};
 
-app.get('/api/auth', async(req, res, next)=> {
-  try {
-    res.send(await User.byToken(req.headers.authorization));
-  }
-  catch(ex){
-    next(ex);
-  }
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.get('/api/users/:id/notes', async (req, res, next) => {
+app.post('/api/auth', async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.params.id);
-    const notes = await user.getNotes();
-    res.send(notes);
+    res.send({ token: await User.authenticate(req.body) });
   }
   catch (ex) {
     next(ex);
   }
 });
 
-app.use((err, req, res, next)=> {
+app.get('/api/auth', requireToken, async (req, res, next) => {
+  try {
+    res.send(await User.byToken(req.headers.authorization));
+  }
+  catch (ex) {
+    next(ex);
+  }
+});
+
+app.get('/api/users/:id/notes', requireToken, async (req, res, next) => {
+  try {
+    if (+req.user.id === +req.params.id) {
+      const user = await User.findByPk(req.params.id);
+      const notes = await user.getNotes();
+      res.send(notes);
+    }
+    else {
+      res.status(403).send('forbidden!');
+    }
+  }
+  catch (ex) {
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).send({ error: err.message });
 });
